d4cc38994768df794755e59a2e38d3ad2c835be2
Felipe Valeriano <101677047+FelipeValeriano21@users.noreply.github.com>
2023-01-08 14:47:13 -0300

    Add files via upload

---
+++ change.js
@@ -0,0 +1,10 @@
+
+
+function mudar(){
+
+const v = document.getElementById ( 'valor' );  
+var square = document.getElementById("tudo");   
+document.getElementById( 'valor' ).style.backgroundColor = v.value;
+
+
+}
